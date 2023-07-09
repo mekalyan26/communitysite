ce006473d70e09635b74fcea20ccf183b2037781
Kalyan Adhikari <adhikari.kalyan@gmail.com>
2023-07-09 17:00:31 -0400

    content added to each of the route page and components in them

--- sitefrontend/src/App.js
+++ sitefrontend/src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Landing from "./routes/Landing"
+import AboutUs from "./routes/AboutUs"
+import Login from "./routes/Login"
+import Register from "./routes/Register"
+import { Routes , Route} from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      
-      </header>
-    </div>
+
+    <Routes>
+      <Route path="/" element={<Landing/>}/>
+      <Route path="/aboutus" element={<AboutUs/>}/>
+      <Route path="/register" element={<Register/>}/>
+      <Route path="/login" element={<Login/>}/>
+    </Routes>
   );
 }
 
