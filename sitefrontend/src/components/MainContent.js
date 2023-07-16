af369ded5cd27d52bb1f5f09d0bb112f2777a7e2
Kalyan Adhikari <adhikari.kalyan@gmail.com>
2023-07-16 08:57:54 -0400

    adding landing page contents

---
+++ sitefrontend/src/components/MainContent.js
@@ -0,0 +1,20 @@
+import "./MainContent.css"
+import React from 'react'
+
+
+const MainContent = () =>{
+    return(
+
+        <div className="container">
+            <div class="item-1">Item 1</div>
+	        <div class="item-2">
+            <img src="../images/social-kolkata"></img>
+            </div>
+	        <div class="item-3">Item 3</div>
+
+        </div>
+
+        )
+};
+
+export default MainContent;
--- sitefrontend/src/routes/Landing.js
+++ sitefrontend/src/routes/Landing.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import MainContent from '../components/MainContent';
 
 const Landing = () => {
   return (
     <div>
       <Navbar/>
+      <MainContent/>
       <Footer />
     </div>
   )
