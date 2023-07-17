702019950df2c3fd3457b33123087c4d4fafcd4d
Kalyan Adhikari <adhikari.kalyan@gmail.com>
2023-07-16 22:01:29 -0400

    added gallery layout

---
+++ sitefrontend/src/routes/Gallery.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CardLayout from '../components/CardLayout';
+
+const Gallery = () => {
+  return (
+    <div>
+      <CardLayout/>
+    </div>
+  )
+}
+
+export default Gallery;
