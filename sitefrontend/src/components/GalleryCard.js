bc6a95093ae8b44fcd3cd8f4bd343bf5916c6eeb
Kalyan Adhikari <adhikari.kalyan@gmail.com>
2023-07-30 19:57:22 -0400

    updating picture gallery

--- sitefrontend/src/components/GalleryCard.js
+++ sitefrontend/src/components/GalleryCard.js
@@ -1,35 +1,22 @@
-import "./GalleryCard.css"
+import "./GalleryCard.css";
+import "../images/socialkolkata.jpg";
 
 
 const GalleryCard = () =>{
     return(
 
-<section class="gallery">
-  <div class="gallery__item">
-    <input type="radio" id="img-1" checked name="gallery" class="gallery__selector"/>
-    <img class="gallery__img" src="https://picsum.photos/id/1015/600/400.jpg" alt=""/>
-    <label for="img-1" class="gallery__thumb">
-        <img src="https://picsum.photos/id/1015/150/100.jpg" alt=""/></label>
+
+  <div className="container1">
+    <div className="heading1">
+        <h3>Photo <span>Gallery</span></h3>
+    </div>
+    <div className="box1">
+        <div className="dream1">
+            <img className ="" src="../images/1.jpg"/>
+        </div>
+    </div>
   </div>
-  <div class="gallery__item">
-    <input type="radio" id="img-2" name="gallery" class="gallery__selector"/>
-    <img class="gallery__img" src="https://picsum.photos/id/1039/600/400.jpg" alt=""/>
-    <label for="img-2" class="gallery__thumb">
-        <img src="https://picsum.photos/id/1039/150/100.jpg" alt=""/></label>
-  </div>
-  <div class="gallery__item">
-    <input type="radio" id="img-3" name="gallery" class="gallery__selector"/>
-    <img class="gallery__img" src="https://picsum.photos/id/1057/600/400.jpg" alt=""/>
-    <label for="img-3" class="gallery__thumb">
-        <img src="https://picsum.photos/id/1057/150/100.jpg" alt=""/></label>
-  </div>
-  <div class="gallery__item">
-    <input type="radio" id="img-4" name="gallery" class="gallery__selector"/>
-    <img class="gallery__img" src="https://picsum.photos/id/106/600/400.jpg" alt=""/>
-    <label for="img-4" class="gallery__thumb">
-        <img src="https://picsum.photos/id/106/150/100.jpg" alt=""/></label>
-  </div>
-</section>
+
  )
 }
 
