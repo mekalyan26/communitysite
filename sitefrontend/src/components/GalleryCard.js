547ad07b1a27bca8411df631bba15975eb2e51e1
Kalyan Adhikari <adhikari.kalyan@gmail.com>
2023-08-02 23:29:10 -0400

    registration page

--- sitefrontend/src/components/GalleryCard.js
+++ sitefrontend/src/components/GalleryCard.js
@@ -6,15 +6,8 @@ const GalleryCard = () =>{
     return(
 
 
-  <div className="container1">
-    <div className="heading1">
-        <h3>Photo <span>Gallery</span></h3>
-    </div>
-    <div className="box1">
-        <div className="dream1">
-            <img className ="" src="../images/1.jpg"/>
-        </div>
-    </div>
+  <div className="main">
+
   </div>
 
  )
--- sitefrontend/src/components/MainContent.js
+++ sitefrontend/src/components/MainContent.js
@@ -5,23 +5,32 @@ import React from 'react'
 const MainContent = () => {
     return (
         <div className="main">
-            <div className="columns">
-                <div className="column">
-                    <h2>2022 Events</h2>
-                    <p>This is where we list all happened last events</p>
-                </div>
-                <div className="column">
-                    <h2>Current Month Events</h2>
-                    <p>Basanta Utsav May 21 - Sponsors contact</p>
-                </div>
-                <div className="column">
-                    <h2>2023 Events</h2>
-                    <p>Community Picnic</p>
-                    <p>Durga Puja</p>
-                </div>
+            <div>
+                <h1>Register</h1>
+                <p>Please fill in this form to create an account.</p>
+
+
+                <label for="email"><b>Email</b></label>
+                <input type="text" placeholder="Enter Email" name="email" id="email" required />
+
+                <label for="psw"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="psw" id="psw" required />
+
+                <label for="psw-repeat"><b>Repeat Password</b></label>
+                <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required/>
+                  
+
+                        <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
+                        <button type="submit" class="registerbtn">Register</button>
+                    </div>
+
+
             </div>
-        </div>
-    )
+
+
+
+
+            )
 };
 
-export default MainContent;
+            export default MainContent;
