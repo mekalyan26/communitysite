26653878aac41ba2d93fc1d9c49ec515cb046452
Kalyan Adhikari <adhikari.kalyan@gmail.com>
2023-08-08 08:33:40 -0400

    updated register component naming convention and other content changes

--- sitefrontend/src/components/Registeration.js
+++ sitefrontend/src/components/Registeration.js
@@ -1,4 +1,4 @@
-import "./MainContent.css"
+import "./Registeration.css"
 import React from 'react'
 
 
@@ -8,8 +8,6 @@ const MainContent = () => {
             <div>
                 <h1>Register</h1>
                 <p>Please fill in this form to create an account.</p>
-
-
                 <label for="email"><b>Email</b></label>
                 <input type="text" placeholder="Enter Email" name="email" id="email" required />
 
@@ -17,20 +15,14 @@ const MainContent = () => {
                 <input type="password" placeholder="Enter Password" name="psw" id="psw" required />
 
                 <label for="psw-repeat"><b>Repeat Password</b></label>
-                <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required/>
-                  
-
-                        <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
-                        <button type="submit" class="registerbtn">Register</button>
-                    </div>
+                <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required />
 
 
+                <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
+                <button type="submit" class="registerbtn">Register</button>
             </div>
-
-
-
-
-            )
+        </div>
+    )
 };
 
-            export default MainContent;
+export default MainContent;
