159662a2c85d627d25898998e005b6aee047d8d0
Kalyan Adhikari <adhikari.kalyan@gmail.com>
2023-08-12 23:55:49 -0400

    updated gallery page

--- sitefrontend/src/components/HomePage.js
+++ sitefrontend/src/components/HomePage.js
@@ -9,7 +9,12 @@ export const HomePage = () => {
         <div className='grid'>
             <div className='img-wrapper1'>
                 <img  src='images/maadurga.jpg' />
-                <div className="content blur fade">Welcome !</div>
+                <div className="content blur fade">
+                    <p>
+                        We are Kitchener Waterloo Cambridge 
+                        Guelp Bengali Cultural Group !
+                    </p>
+                </div>
             </div>
            
         </div>
