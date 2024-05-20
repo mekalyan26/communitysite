402483e9fe331d321c131c5975fbcae1e21c9239
Kalyan Adhikari <adhikari.kalyan@gmail.com>
2024-05-20 14:32:56 -0400

    layout and content changes
    updated home page layout and content added section,
    updated Navbar main text color

--- sitefrontend/src/components/pages/Home/HomePage.js
+++ sitefrontend/src/components/pages/Home/HomePage.js
@@ -1,23 +1,35 @@
-import React from 'react'
+import React from "react";
 import "./HomePage.css";
 
 export const HomePage = () => {
-    return (
+  return (
+    <div className="grid">
+        
+      <section>
+        <h5 className='section-text'> !! Announcement !!</h5>
+        <p >
+          KWGC Bengali Cultural Group needs more volunteers.
+          Want to be part of the vibrant culture , come join us
+          email : kwgcbengaliculturalgroup@gmail.com
+        </p>
+      </section>
 
-        <div className='grid'>
-            <div className='img-wrapper1'>
-                <img  src='images/maadurga.jpg' alt=''/>
-                <div className="content blur fade">
-                    <p>
-                        We are Kitchener Waterloo Cambridge 
-                        Guelp Bengali Cultural Group !
-                    </p>
-                </div>
-            </div>
-           
-        </div>
+      <section>
+        <h5 className='section-text'>Upcomming Event</h5>
+        <p>
+         Summer Picnic 2024 
+        </p>
+      </section>
 
-    )
-}
+      <section>
+        
+        <h5  className='section-text'>For Sponsorship</h5>
+        <p>
+        <i>Email</i> :  kwgcbengaliculturalgroup@gmail.com
+        </p>
+      </section>
+    </div>
+  );
+};
 
 export default HomePage;
